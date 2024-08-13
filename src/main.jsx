095e531279c38d232396ac31374b756a3fe4644c
Bean <bean@Beans-MBP.lan>
2024-08-13 19:02:30 -0400

    adding page layout and reacty router:

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <About />,
+    //   },
+    //   {
+    //     path: 'portfolio',
+    //     element: <Portfolio />,
+    //   },
+    //   {
+    //     path: 'contact',
+    //     element: <Contact />,
+    //   },
+    //   {
+    //     path: 'resume',
+    //     element: <Resume />,
+    //   },
+    // ],
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
